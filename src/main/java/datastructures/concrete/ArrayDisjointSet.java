6067de37773af385091c83c8c8368d15bf08d9a7
Pranav Kartha <karthp@uw.edu>
2018-12-03 19:18:44 -0800

    updated

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -12,59 +12,68 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     // Note: do NOT rename or delete this field. We will be inspecting it
     // directly within our private tests.
     private int[] pointers;
-    private IDictionary<Integer, Integer> ranks;
-    //  note: might just be totally vestigial... FIX IF NEEDED
+    private int counter;
+    private IDictionary<T, Integer> converter;
     
-    private static final int STARTING_SIZE = 100;
+    private static final int STARTING_SIZE = 10;
 
     // However, feel free to add more methods and private helper methods.
     // You will probably need to add one or two more fields in order to
     // successfully implement this class.
 
     public ArrayDisjointSet() {
-        this.pointers = new int[STARTING_SIZE];
-        this.ranks = new ChainedHashDictionary<>();
+        this.pointers = this.makeNewArray(STARTING_SIZE);
+        this.counter = 0;
+        this.converter = new ChainedHashDictionary<>();
+    }
+    
+    private int[] makeNewArray(int size) {
+        //  "empty" slots of array filled with value of array length
+        int[] bic = new int[size];
+        for (int i = 0; i < size; i++) {
+            bic[i] = size;
+        }
+        return bic;
     }
 
     @Override
     public void makeSet(T item) {
-
-        //  assuming all hash codes are unique and positive
-        int code = item.hashCode();
+        //  codes based on count
+        int code = this.counter;
         if (code >= this.pointers.length) {
-            this.resize(code + 1);
+            this.resize(this.pointers.length * 2);
         }
         
-        if (this.pointers[code] != null) { 
-            //  what if 0 is a parent
-            //  fix later...
+        int emptySlot = this.pointers.length;
+        if (this.pointers[code] != emptySlot) { 
             throw new IllegalArgumentException();
         }
         
-        //  item is new to Set   
-        // add -1 to code index
-        // put code, 1 to ranks
+        //  item is new to Set
+        //  add -1 to code index
         this.pointers[code] = -1;
-        this.ranks.put(code, 1);
+        this.converter.put(item, code);
+        this.counter++;
     }
     
     private void resize(int newSize) {
-        int[] newBoi = new int[newSize];
-        for (int i = 0; i < this.pointers.length; i++) {
-            newBoi[i] = this.pointers[i];
+        int[] newBoi = this.makeNewArray(newSize);
+        int oldSize = this.pointers.length;
+        for (int i = 0; i < oldSize; i++) {
+            if (this.pointers[i] != oldSize) {
+                newBoi[i] = this.pointers[i];
+            }
         }
         this.pointers = newBoi;
     }
 
     @Override
     public int findSet(T item) {
-        int code = item.hashCode();
-        
-        //  0 is default initial value.... for now
-        if (this.pointers.length >= code || this.pointers[code] == 0) {
+        if (!this.converter.containsKey(item)) {
             throw new IllegalArgumentException();
         }
-        //  ting should never have a parent with index 0...
+        
+        int code = this.converter.get(item);
         
         //  find parent of ting
         return this.findParent(code);
